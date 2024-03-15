2a7e1a42360f7b21aef04fa1453dcc302b52ccca
herve.ngisse <125308992+h-mbl@users.noreply.github.com>
2024-03-15 11:36:55 -0400

    init

---
+++ Q2 Escalade/cmake-build-debug/CMakeFiles/Q2_Escalade.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/herve/OneDrive/Documents/GitHub/IFT2125_TP3/Q2 Escalade/ClimbingDifficultyCalculator.cpp" "CMakeFiles/Q2_Escalade.dir/ClimbingDifficultyCalculator.cpp.o" "gcc" "CMakeFiles/Q2_Escalade.dir/ClimbingDifficultyCalculator.cpp.o.d"
+  "/mnt/c/Users/herve/OneDrive/Documents/GitHub/IFT2125_TP3/Q2 Escalade/climbing_difficulty.cpp" "CMakeFiles/Q2_Escalade.dir/climbing_difficulty.cpp.o" "gcc" "CMakeFiles/Q2_Escalade.dir/climbing_difficulty.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Q2 Escalade/cmake-build-debug/CMakeFiles/Q2_Escalade.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q2_Escalade.dir/ClimbingDifficultyCalculator.cpp.o"
+  "CMakeFiles/Q2_Escalade.dir/ClimbingDifficultyCalculator.cpp.o.d"
+  "CMakeFiles/Q2_Escalade.dir/climbing_difficulty.cpp.o"
+  "CMakeFiles/Q2_Escalade.dir/climbing_difficulty.cpp.o.d"
+  "Q2_Escalade"
+  "Q2_Escalade.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Q2_Escalade.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
